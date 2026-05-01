1e62eb61a6a9f69bb9bcfe2a7552bd6590b849a7
sairohith286 <galamsairohith286@gmail.com>
2026-05-01 15:16:51 +0530

    Create CampusPeoplePortalApplication.java

---
+++ src/main/java/com/demo/campus_people_portal/CampusPeoplePortalApplication.java
@@ -0,0 +1,12 @@
+package com.demo.campus_people_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPeoplePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPeoplePortalApplication.class, args);
+    }
+}
